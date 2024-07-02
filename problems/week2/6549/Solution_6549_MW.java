0ba5588a1a15a828b89e8bec150186467dc67e7a
k-kmw <kimmw2_@naver.com>
2024-07-02 23:43:51 +0900

    Solve 6549 주석 추가

--- problems/week2/6549/Solution_6549_MW.java
+++ problems/week2/6549/Solution_6549_MW.java
@@ -10,12 +10,12 @@ public class Solution_6549_MW {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         while(true) {
-            String[] s = br.readLine().split(" ");
-            n = Integer.parseInt(s[0]);
-            if(n == 0)  break;
-            h = new int[n];
-            for(int i=0; i<n; i++) {
-                h[i] = Integer.parseInt(s[i+1]);
+                    String[] s = br.readLine().split(" ");
+                    n = Integer.parseInt(s[0]);
+                    if(n == 0)  break;
+                    h = new int[n];
+                    for(int i=0; i<n; i++) {
+                        h[i] = Integer.parseInt(s[i+1]);
             }
 
             long ans = getMaxArea(0, n-1);
@@ -33,12 +33,12 @@ public class Solution_6549_MW {
 
         int mid = (st+en) / 2;
 
-        long left = getMaxArea(st, mid);
-        long right = getMaxArea(mid+1, en);
+        long left = getMaxArea(st, mid); // 중간을 기준으로 왼쪽의 최대 넓이
+        long right = getMaxArea(mid+1, en); // 중간을 기준으로 오른쪽 최대 넓이
 
         long mx = Math.max(left, right);
 
-        mx = Math.max(mx, getMiddleArea(st, en));
+        mx = Math.max(mx, getMiddleArea(st, en)); // 중간을 포함하는 최대 넓이
 
         return mx;
     }
@@ -53,7 +53,7 @@ public class Solution_6549_MW {
         long area;
         long mx = h[mid];
 
-        while(lo > st && ho < en) {
+        while(lo > st && ho < en) { // 좌/우 하나라도 끝에 도달할 때까지 반복
 
             if(h[lo-1] > h[ho+1]) {
                 lo--;
@@ -67,14 +67,14 @@ public class Solution_6549_MW {
             mx = Math.max(mx, area);
         }
 
-        while(lo > st) {
+        while(lo > st) { // 왼쪽 끝까지 도달하도록 함
             lo--;
             height = Math.min(height, h[lo]);
             area = height * (ho - lo + 1);
             mx = Math.max(mx, area);
         }
 
-        while(ho < en) {
+        while(ho < en) { // 오른쪽 끝까지 도달하도록 함
             ho++;
             height = Math.min(height, h[ho]);
             area = height * (ho - lo + 1);
